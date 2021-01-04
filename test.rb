2cb77d0853ae2ef7b1890ae48ef29760fa24d977
ivannaht <ivhontaryk@gmail.com>
2021-01-04 17:41:21 +0200

    authentication

--- test.rb
+++ test.rb
@@ -4,8 +4,10 @@ driver = Selenium::WebDriver.for :chrome
 driver.navigate.to 'http://automationpractice.com/index.php'
 driver.manage.window.maximize
 driver.find_element(:class, 'login').click
+authentication = driver.find_element(:class, 'page-heading').text
+puts (authentication)
 driver.find_element(:id, 'email_create').send_keys('testmail200@gmail.com')
-
+driver.find_element(:id, 'SubmitCreate').click
 
 sleep(5)
 
